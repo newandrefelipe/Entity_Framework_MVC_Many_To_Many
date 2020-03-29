bba91929bc67a94854aaa2ffaf335b4292bcb7c3
Andre Felipe dos Santos <newandrefelipe@gmail.com>
2020-03-29 18:03:16 -0300

    StudentDAO and examples added

---
+++ Entity_Framework_MVC_Many_To_Many/DAO/StudentDAO.cs
@@ -0,0 +1,61 @@
+﻿using Entity_Framework_MVC_Many_To_Many.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Framework_MVC_Many_To_Many.DAO
+{
+    public class StudentDAO
+    {
+        public void Add(Student student)
+        {
+            using (var context = new ManyToManyContext())
+            {
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+        }
+
+        public IList<Student> ListAll()
+        {
+            using (var context = new ManyToManyContext())
+            {
+                return context.Students.ToList();
+            }
+        }
+
+        public Student FindById(int id)
+        {
+            using (var context = new ManyToManyContext())
+            {
+                var student = context
+                    .Students
+                    .Include("Courses")
+                    .Where(s => s.Id == id)
+                    .FirstOrDefault();
+
+                return student;
+            }
+        }
+
+        public void Update(Student student)
+        {
+            using (var context = new ManyToManyContext())
+            {
+                context.Entry(student).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void Remove(Student student)
+        {
+            using (var context = new ManyToManyContext())
+            {
+                context.Students.Remove(student);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Entity_Framework_MVC_Many_To_Many/Program.cs
+++ Entity_Framework_MVC_Many_To_Many/Program.cs
@@ -13,6 +13,83 @@ namespace Entity_Framework_MVC_Many_To_Many
         static void Main(string[] args)
         {
             Console.WriteLine("Starting");
+
+            //AddCourses();
+            //AddStudents();
+            UpdateWithRelationship();
+            //AddWithRelationship();
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static void AddWithRelationship()
+        {
+            var course = new Course()
+            {
+                Name = "Test Course"
+            };
+
+            var student = new Student()
+            {
+                Name = "Test Student",
+            };
+
+            student.Courses.Add(course);
+
+            var courseDAO = new CourseDAO();
+            var studentDAO = new StudentDAO();
+
+            courseDAO.Add(course);
+            studentDAO.Add(student);
+        }
+
+        private static void UpdateWithRelationship()
+        {
+            var courseDAO = new CourseDAO();
+            var studentDAO = new StudentDAO();
+
+            var course1 = courseDAO.FindById(1);
+            var course2 = courseDAO.FindById(2);
+
+            var student1 = studentDAO.FindById(1);
+            student1.Courses.Add(course1);
+            student1.Courses.Add(course2);
+
+            studentDAO.Update(student1);
+        }
+
+        private static void AddStudents()
+        {
+            var student1 = new Student()
+            {
+                Name = "André Felipe"
+            };
+
+            var student2 = new Student()
+            {
+                Name = "João da Silva"
+            };
+
+            var student3 = new Student()
+            {
+                Name = "Mario Gomes"
+            };
+
+            var student4 = new Student()
+            {
+                Name = "Maria Santos"
+            };
+
+            var dao = new StudentDAO();
+            dao.Add(student1);
+            dao.Add(student2);
+            dao.Add(student3);
+            dao.Add(student4);
+        }
+
+        private static void AddCourses()
+        {
             var course1 = new Course()
             {
                 Name = "Entity Framework 6"
@@ -32,10 +109,6 @@ namespace Entity_Framework_MVC_Many_To_Many
             dao.Add(course1);
             dao.Add(course2);
             dao.Add(course3);
-
-            Console.WriteLine("Done");
-
-            Console.ReadKey();
         }
     }
 }
